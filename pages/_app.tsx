2644dc0fca1a5a444d188a3375d957c9f7e323d9
forecho <caizhenghai@gmail.com>
2023-01-08 12:21:10 +0800

    feat: add nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+  );
 }
