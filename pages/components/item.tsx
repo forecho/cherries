db67f8c22bceb74b7214d2b37ea71f959bad9b47
forecho <caizhenghai@gmail.com>
2023-01-08 23:43:48 +0800

    feat: add link

--- pages/components/item.tsx
+++ pages/components/item.tsx
@@ -1,28 +1,31 @@
-import { Card, Grid, Text, Link } from "@nextui-org/react";
+import {Card, Grid, Text, Link} from "@nextui-org/react";
 import {ResourceItem} from "../api";
 import Image from 'next/image'
 
 export default function Item(item: ResourceItem) {
     return (
-        <Card css={{ p: "$6", mw: "400px" }}>
-            <Card.Header>
-                <Image
-                    alt={item.title}
-                    src={item.logo}
-                    width="34"
-                    height="34"
-                />
-                <Grid.Container css={{ pl: "$6" }}>
-                    <Grid xs={12}>
-                        <Text h4 css={{ lineHeight: "$xs" }}>
-                            {item.title}
-                        </Text>
-                    </Grid>
-                    <Grid xs={12}>
-                        <Text css={{ color: "$accents8" }}>{item.description}</Text>
-                    </Grid>
-                </Grid.Container>
-            </Card.Header>
-        </Card>
+            <Card isPressable css={{p: "$6", mw: "400px"}}>
+                <Link href={item.url} target="_blank">
+                <Card.Header>
+                    <Image
+                        alt={item.title}
+                        src={item.logo}
+                        width="34"
+                        height="34"
+                    />
+                    <Grid.Container css={{pl: "$6"}}>
+                        <Grid xs={12}>
+                            <Text h4 css={{lineHeight: "$xs"}}>
+                                {item.title}
+                            </Text>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Text css={{color: "$accents8"}}>{item.description}</Text>
+                        </Grid>
+                    </Grid.Container>
+                </Card.Header>
+                </Link>
+
+            </Card>
     );
 }
