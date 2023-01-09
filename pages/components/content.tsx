3c9020ca8f44f98f4cb2e645b08a70bba4827013
forecho <caizhenghai@gmail.com>
2023-01-09 22:45:31 +0800

    fix: fix grid

--- pages/components/content.tsx
+++ pages/components/content.tsx
@@ -1,5 +1,5 @@
 import Banner from "./banner";
-import {Text, Grid, Link} from "@nextui-org/react";
+import {Text, Grid, Link, Container} from "@nextui-org/react";
 import {resources} from "../api";
 import Item from "./item";
 import {Box} from "./box";
@@ -8,34 +8,36 @@ import Footer from "./footer";
 export default function Content() {
     return (
         <Box css={{px: "$12", mt: "$8", "@xsMax": {px: "$10"}}}>
-            <Banner/>
-            <Grid.Container gap={2}>
-                {
-                    resources.map((resource, i) => {
-                        return (
-                            <>
-                                <Grid xs={12} id={resource.title} className="title">
-                                    <Link href={`#${resource.title}`}>
-                                        <Text h3>
-                                            {resource.title}
-                                        </Text>
-                                    </Link>
-                                </Grid>
-                                {
-                                    resource.site.map((site, index) => {
-                                        return (
-                                            <Grid xs={12} sm={6} md={4} lg={3} key={index}>
-                                                <Item {...site}/>
-                                            </Grid>
-                                        )
-                                    })
-                                }
-                            </>
-                        )
-                    })
-                }
-                <Footer/>
-            </Grid.Container>
+            <Container>
+                <Banner/>
+                <Grid.Container gap={2}>
+                    {
+                        resources.map((resource, i) => {
+                            return (
+                                <>
+                                    <Grid xs={12} id={resource.title} className="title">
+                                        <Link href={`#${resource.title}`}>
+                                            <Text h3>
+                                                {resource.title}
+                                            </Text>
+                                        </Link>
+                                    </Grid>
+                                    {
+                                        resource.site.map((site, index) => {
+                                            return (
+                                                <Grid xs={12} sm={6} md={4} lg={3} key={index}>
+                                                    <Item {...site}/>
+                                                </Grid>
+                                            )
+                                        })
+                                    }
+                                </>
+                            )
+                        })
+                    }
+                    <Footer/>
+                </Grid.Container>
+            </Container>
         </Box>
     );
 }
